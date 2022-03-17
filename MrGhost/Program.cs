91dad6c24ba3ec81c07591c28865ca70c5ba16fe
grantpinkham <grantpinkham@yahoo.com>
2022-03-17 16:27:41 -0500

    Initial commit

---
+++ MrGhost/Program.cs
@@ -0,0 +1,17 @@
+using System;
+namespace MrGhost
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // console.writelines of a mr. ghost
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("-Mr. Ghost");
+        }
+    }
+}
