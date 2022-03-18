4291be6cbc1e841f8fa18fad47b65bb6b61c30a1
grantpinkham <grantpinkham@yahoo.com>
2022-03-18 09:09:51 -0500

    add killer beetle caterpillar

---
+++ KillerBeetleCaterpillar/Program.cs
@@ -0,0 +1,19 @@
+using System;
+namespace KillerBeetleCaterpillar
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // console.writelines drawing a killer beetle caterpillar
+            Console.WriteLine("   /   \\\\");
+            Console.WriteLine("   \\\\. ./");
+            Console.WriteLine("  (o + o)");
+            Console.WriteLine("--|  |  |--");
+            Console.WriteLine("--|  |  |--");
+            Console.WriteLine("--|  |  |--");
+            Console.WriteLine("   v   v");
+            Console.WriteLine("   *****");
+        }
+    }
+}
