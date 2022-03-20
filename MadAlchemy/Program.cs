29d6957493562aa5571d1a7b1c4cc0154dc07c3f
grantpinkham <grantpinkham@yahoo.com>
2022-03-20 10:24:14 -0500

    add mad alchemy

---
+++ MadAlchemy/Program.cs
@@ -0,0 +1,21 @@
+using System;
+namespace MadAlchemy
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // console.writelines drawing alchemy gone wrong
+            Console.WriteLine("     (    )");
+            Console.WriteLine("  (  (   )  )");
+            Console.WriteLine("(  (       )  )");
+            Console.WriteLine("  (  (   )    )");
+            Console.WriteLine("     (     )");
+            Console.WriteLine("    /--------\\\\");
+            Console.WriteLine("   /   \\\\   /  \\\\");
+            Console.WriteLine("  /     0 0    \\\\");
+            Console.WriteLine(" /       o      \\\\");
+            Console.WriteLine(" ****************");
+        }
+    }
+}
