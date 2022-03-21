aa3d94b28dd6df5f95ef444640c32c5725bcb16d
grantpinkham <grantpinkham@yahoo.com>
2022-03-21 03:10:58 -0500

    add gingerbread man

---
+++ GingerbreadMan/Program.cs
@@ -0,0 +1,16 @@
+using System;
+namespace GingerbreadMan
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // console.writelines drawing a gingerbread man
+            Console.WriteLine("   _   ");
+            Console.WriteLine(" _(\")_ ");
+            Console.WriteLine("(_ . _)");
+            Console.WriteLine(" / : \\ ");
+            Console.WriteLine("(_/ \\_)");
+        }
+    }
+}
