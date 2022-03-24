8867df1bf5830700c6da845c205fb2705e947a9a
grantpinkham <grantpinkham@yahoo.com>
2022-03-24 13:41:10 -0500

    add contemplative bear

---
+++ ContemplativeBear/Program.cs
@@ -0,0 +1,24 @@
+using System;
+namespace ContemplativeBear
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // console.writelines drawing a contemplative bear
+            Console.WriteLine("   _");
+            Console.WriteLine(" _( )_");
+            Console.WriteLine("(     (o___");
+            Console.WriteLine(" |      _ 7");
+            Console.WriteLine("  \\    (\")");
+            Console.WriteLine("  /     \\ \\");
+            Console.WriteLine(" |    \\ __/");
+            Console.WriteLine(" |        |");
+            Console.WriteLine(" (       /");
+            Console.WriteLine("  \\     /");
+            Console.WriteLine("   )   /(_");
+            Console.WriteLine("   |  (___)");
+            Console.WriteLine("    \\___)");
+        }
+    }
+}
