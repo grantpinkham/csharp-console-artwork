1c2fcaf8d99e587e359ef09bc6f5c8f1115533ab
grantpinkham <grantpinkham@yahoo.com>
2022-03-28 17:26:59 -0500

    add sparkling water

---
+++ SparklingWater/Program.cs
@@ -0,0 +1,22 @@
+using System;
+namespace SparklingWater
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // console.writelines drawing sparkling water
+            Console.WriteLine("   .");
+            Console.WriteLine("  .");
+            Console.WriteLine(" . .");
+            Console.WriteLine("  ...");
+            Console.WriteLine("\\~~~~~/");
+            Console.WriteLine(" \\   /");
+            Console.WriteLine("  \\ /");
+            Console.WriteLine("   V");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("  ---");
+        }
+    }
+}
